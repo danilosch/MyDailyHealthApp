3825049b83b5caca48caf3aca1ab9471c9c675d7
Danilo Schervinski <danilo_sch@live.com>
2024-11-01 11:17:12 -0300

    feat: adiciona fonte customizada

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
+import {
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "./src/screens/Home";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    light: Inter_300Light,
+    regular: Inter_400Regular,
+    semibold: Inter_600SemiBold,
+    bold: Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
       <Home />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
